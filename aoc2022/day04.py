51a89123ff62cac80077a29425c1db2091d2a11b
Daniel Halligan <dan.halligan@gmail.com>
2022-12-04 07:06:12 +0000

    Adds day 4

--- aoc2022/day04.py
+++ aoc2022/day04.py
@@ -1,6 +1,20 @@
+from aoc2022.helpers import input_lines, ints
+import re
+
+
+def contains(x1, x2, y1, y2):
+    return (y1 >= x1 and y2 <= x2) or (x1 >= y1 and x2 <= y2)
+
+
+def overlaps(x1, x2, y1, y2):
+    return (y1 >= x1 and y1 <= x2) or (x1 >= y1 and x1 <= y2)
+
+
 def part1(file):
-    return None
+    dat = [ints(re.findall("\\d+", line)) for line in input_lines(file)]
+    return sum(contains(*x) for x in dat)
 
 
 def part2(file):
-    return None
+    dat = [ints(re.findall("\\d+", line)) for line in input_lines(file)]
+    return sum(overlaps(*x) for x in dat)
--- tests/test_aoc2022.py
+++ tests/test_aoc2022.py
@@ -1,7 +1,12 @@
 import importlib
 import pytest
 
-solutions = [(1, [24000, 45000]), (2, [15, 12]), (3, [157, 70])]
+solutions = [
+    (1, [24000, 45000]),
+    (2, [15, 12]),
+    (3, [157, 70]),
+    (4, [2, 4]),
+]
 
 
 # Test each day by importing the module and running part1 and part2
