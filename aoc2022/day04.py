cc9a2241d8144dfc2126b11a8d457711718fd275
Daniel Halligan <dan.halligan@gmail.com>
2022-12-04 16:26:45 +0000

    Day4 tidy

--- aoc2022/day04.py
+++ aoc2022/day04.py
@@ -2,6 +2,10 @@ from aoc2022.helpers import input_lines, ints
 import re
 
 
+def data(file):
+    return [ints(re.findall("\\d+", line)) for line in input_lines(file)]
+
+
 def contains(x1, x2, y1, y2):
     return (y1 >= x1 and y2 <= x2) or (x1 >= y1 and x2 <= y2)
 
@@ -11,10 +15,8 @@ def overlaps(x1, x2, y1, y2):
 
 
 def part1(file):
-    dat = [ints(re.findall("\\d+", line)) for line in input_lines(file)]
-    return sum(contains(*x) for x in dat)
+    return sum(contains(*x) for x in data(file))
 
 
 def part2(file):
-    dat = [ints(re.findall("\\d+", line)) for line in input_lines(file)]
-    return sum(overlaps(*x) for x in dat)
+    return sum(overlaps(*x) for x in data(file))
