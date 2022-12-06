74f95660283a609a9ae900fb38c0b32c3b7c3933
Daniel Halligan <dan.halligan@gmail.com>
2022-12-06 06:13:45 +0000

    Day 6

--- aoc2022/day06.py
+++ aoc2022/day06.py
@@ -1,6 +1,17 @@
+def data(file):
+    return open(file).read().rstrip()
+
+
+def marker(dat, size):
+    for i in range(len(dat) - size + 1):
+        if len(set(dat[i : i + size])) == size:
+            break
+    return i + size
+
+
 def part1(file):
-    return None
+    return marker(data(file), 4)
 
 
 def part2(file):
-    return None
+    return marker(data(file), 14)
