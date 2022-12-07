ef7a5cf13a129a0fc18d3732279c87e64a17d052
Daniel Halligan <dan.halligan@gmail.com>
2022-12-07 09:44:08 +0000

    Tidying

--- aoc2022/day07.py
+++ aoc2022/day07.py
@@ -1,38 +1,38 @@
 from aoc2022.helpers import input_lines
-import re
+
+
+def path(tree):
+    return "/".join(tree)
 
 
 def parse(file):
     dat = input_lines(file)
-    files = {"/": {"f": {}, "d": [], "p": None}}
+    files = {"/": {"f": {}, "d": []}}
     dirs = []
-    pos = 1
-    curr = "/"
-    while pos < len(dat):
-        if dat[pos] == "$ ls":
-            pos += 1
-            while pos < len(dat) and re.match(r"^[^\$]", dat[pos]):
-                if re.match(r"^\d+", dat[pos]):
-                    size, n = dat[pos].split()
-                    files[curr]["f"][n] = int(size)
-                elif re.match(r"^dir", dat[pos]):
-                    _, n = dat[pos].split()
-                    files[curr]["d"] += [f"{curr}/{n}"]
-                    dirs += [f"{curr}/{n}"]
-                pos += 1
-        elif dat[pos] == "$ cd ..":
-            curr = files[curr]["p"]
-            pos += 1
-        elif re.match(r"^\$ cd", dat[pos]):
-            _, _, n = dat[pos].split()
-            files[f"{curr}/{n}"] = {"f": {}, "d": [], "p": None}
-            files[f"{curr}/{n}"]["p"] = curr
-            curr = f"{curr}/{n}"
-            pos += 1
+    i = 1
+    tree = ["/"]
+    while i < len(dat):
+        if dat[i] == "$ ls":
+            i += 1
+            while i < len(dat) and not dat[i].startswith("$"):
+                if dat[i].startswith("dir"):
+                    n = dat[i].split()[1]
+                    files[path(tree)]["d"] += [path(tree + [n])]
+                else:
+                    size, n = dat[i].split()
+                    files[path(tree)]["f"][n] = int(size)
+                i += 1
+        elif dat[i] == "$ cd ..":
+            tree = tree[:-1]
+            i += 1
+        else:
+            tree += [dat[i].split()[2]]
+            files[path(tree)] = {"f": {}, "d": []}
+            dirs += [path(tree)]
+            i += 1
     return files, dirs
 
 
-# recursively sum directories
 def dirsize(files, name):
     s1 = sum(x for x in files[name]["f"].values())
     s2 = sum(dirsize(files, n) for n in files[name]["d"])
@@ -48,6 +48,5 @@ def part1(file):
 def part2(file):
     files, dirs = parse(file)
     sizes = {n: dirsize(files, n) for n in ["/"] + dirs}
-    unused = 70000000 - sizes["/"]
-    to_free = 30000000 - unused
+    to_free = 30000000 - (70000000 - sizes["/"])
     return min(s for x, s in sizes.items() if s >= to_free)
